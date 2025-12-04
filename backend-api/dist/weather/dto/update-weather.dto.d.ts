dd521bd0595ce4424f12a8d3f0e51cce6748b2e6
PHdaCruzSantos <phdacruzsantos.dev@gmail.com>
2025-12-04 15:19:53 -0300

    chore: remove dist files from version control

--- backend-api/dist/weather/dto/update-weather.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateWeatherDto } from './create-weather.dto';
-declare const UpdateWeatherDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreateWeatherDto>>;
-export declare class UpdateWeatherDto extends UpdateWeatherDto_base {
-}
-export {};
